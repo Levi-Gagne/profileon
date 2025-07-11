f4f711299d9322683275b4c81ff880a2ac3647f3
Levi Gagne <67981022+Levi-Gagne@users.noreply.github.com>
2025-07-11 08:46:29 -0400

    Create WORKING.py

---
+++ WORKING.py
@@ -0,0 +1,10 @@
+So here are some extrcts from the brandguidlines to keep in mind
+
+mayve we keep or create a refernce for this? thinking that for example we have log guilines, typoography color rules etc 
+
+
+these should be in natural language so i can pass to my ai when desinign my app ui, so please help me synthese descriptions that are very detialed and written in a way to help create guidlines for my ai to follow in app creation
+
+
+here are portions i see that we can modify: 
+                                  
